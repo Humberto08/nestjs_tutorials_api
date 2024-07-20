dc3dcf08af39f741675bc4c119a2c0d172690770
Humberto Luciano <humberto_luciano@hotmail.com>
2024-07-20 18:31:59 -0300

    feat: Implementing Pipes, Body and Validations

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
+import { CreateUserInputDTO } from './dtos/createUserInput.dto';
+import { UpdateUserInputDTO } from './dtos/updateUserInput.dto';
+
+
 
 @Controller('users')
 export class UsersController {
   @Get()
-  findAll(@Query('id') id: number) {
+  findAll(@Query('id')  id: number) {
     if (id) {
       return 'Olá essa é a rota /users' + id;
     }
@@ -12,18 +16,18 @@ export class UsersController {
   }
 
   @Get(':id')
-  findById(@Param('id') id: number) {
+  findById(@Param('id', new ParseIntPipe) id: number) {
     return id;
   }
 
   @Post()
-  create() {
-    return 'Create user';
+  create(@Body() body: CreateUserInputDTO) {
+    return body;
   }
 
   @Put(':id')
-  update(@Param('id') id: number) {
-    return 'Update user ' + id;
+  update(@Param('id') id: number, @Body() body: UpdateUserInputDTO) {
+    return body;
   }
 
   @Delete(':id')
